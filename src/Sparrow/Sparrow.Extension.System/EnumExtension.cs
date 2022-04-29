477d837b1519ffb69e8a80eee5cac6223454b9c5
zheng gan <932813593@qq.com>
2022-04-29 11:55:13 +0800

    🐞 fix: 修复枚举扩展bug

--- src/Sparrow/Sparrow.Extension.System/EnumExtension.cs
+++ src/Sparrow/Sparrow.Extension.System/EnumExtension.cs
@@ -16,11 +16,26 @@ namespace Sparrow.Extension.System
         /// <returns></returns>
         public static string GetDescription(this Enum value)
         {
+            return value.GetDescription(default);
+        }
+
+        /// <summary>
+        /// 获取枚举描述
+        /// </summary>
+        /// <param name="value">枚举值</param>
+        /// <param name="defaultDescription">默认描述</param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value, string defaultDescription)
+        {
             if (value == null)
             {
                 return string.Empty;
             }
             var field = value.GetType().GetField(value.ToString());
+            if (field is null)
+            {
+                return defaultDescription;
+            }
             var description = ((DescriptionAttribute)field.GetCustomAttribute(typeof(DescriptionAttribute)))?.Description;
             return !string.IsNullOrEmpty(description) ? description : field.Name;
         }
