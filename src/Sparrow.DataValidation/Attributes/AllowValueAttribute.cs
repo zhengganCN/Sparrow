e5b9acdb2e29ae73823375d6911287332699b24e
zheng gan <932813593@qq.com>
2022-06-02 09:38:49 +0800

    ✨ feat: 新增自定义函数验证特性

--- src/Sparrow.DataValidation/Attributes/AllowValueAttribute.cs
+++ src/Sparrow.DataValidation/Attributes/AllowValueAttribute.cs
@@ -8,7 +8,7 @@ namespace Sparrow.DataValidation.Attributes
     /// 允许的值
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class AllowValueAttribute : ValidationAttribute
+    public sealed class AllowValueAttribute : ValidationAttribute
     {
         private readonly object[] _values;
         public AllowValueAttribute(params object[] values)
---
+++ src/Sparrow.DataValidation/Attributes/FunctionAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sparrow.DataValidation.Attributes
+{
+    /// <summary>
+    /// 自定义函数验证接口
+    /// </summary>
+    public interface IFunction
+    {
+        /// <summary>
+        /// 自定义验证函数
+        /// </summary>
+        /// <param name="value">需验证的值</param>
+        /// <returns></returns>
+        bool Valid(object value);
+    }
+
+    /// <summary>
+    /// 自定义函数验证特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class FunctionAttribute : ValidationAttribute
+    {
+        private readonly IFunction ValidClass;
+        /// <summary>
+        /// 自定义函数验证
+        /// </summary>
+        /// <param name="type">必须实现<see cref="IFunction"/>接口</param>
+        /// <exception cref="ArgumentException"></exception>
+        public FunctionAttribute(Type type)
+        {
+            if (type.GetInterface(nameof(IFunction)) is null)
+            {
+                throw new ArgumentException("未实现IFunction接口");
+            }
+            ValidClass = Activator.CreateInstance(type) as IFunction;
+        }
+
+        public override bool IsValid(object value)
+        {            
+            return ValidClass.Valid(value);
+        }
+    }
+}
